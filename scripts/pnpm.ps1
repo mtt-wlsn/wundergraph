b62d35fdb6fc6324d86ae7c93d1e5e2168fd145d
Alberto García Hierro <alberto@garciahierro.com>
2023-01-08 21:26:56 +0000

    feat: experimental examples CI on Windows

---
+++ scripts/pnpm.ps1
@@ -0,0 +1,8 @@
+# Bootstrap pnpm workspace
+
+# Installwithout scripts because wunderctl has to be build first
+pnpm install --ignore-scripts
+# Build wunderctl
+pnpm -r run --filter="./packages/wunderctl" build
+# Build all libs + link all packages
+pnpm build:libs && pnpm i
